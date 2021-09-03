72fde6f01cbb9123884e512e8ae314f499061dbd
it-school <GITHUB it@osvita.od.ua>
2021-09-03 09:02:50 +0300

    2

--- src/itschool/MainClass.java
+++ src/itschool/MainClass.java
@@ -1,10 +1,17 @@
 package itschool;
 
-public class Main {
-
-   public static void main(String[] args) {
+/**
+ * Main class
+ */
+public class MainClass {
+   /**
+    * Method main
+    *
+    * @param args cmd line args
+    */
+   public static void main(final String[] args) {
       System.out.println("eye5r6ufyjgku");
-      System.out.println("affsgzdhtxfjgcjhvk");
+      System.out.println("hello");
       System.out.println("affsgzdhtxfjgcjhvk");
       System.out.println("affsgzdhtxfjgcjhvk");
       System.out.println("affsgzdhtxfjgcjhvk");
