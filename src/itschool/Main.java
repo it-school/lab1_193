b36d59c2370abbd14167a89e9dd443fdb2ca705f
it-school <GITHUB it@osvita.od.ua>
2021-09-03 08:56:20 +0300

    1

--- src/itschool/Main.java
+++ src/itschool/Main.java
@@ -3,7 +3,13 @@ package itschool;
 public class Main {
 
    public static void main(String[] args) {
-      System.out.println("affsgzdhtxfjgcjhvk");   
+      System.out.println("eye5r6ufyjgku");
+      System.out.println("affsgzdhtxfjgcjhvk");
+      System.out.println("affsgzdhtxfjgcjhvk");
+      System.out.println("affsgzdhtxfjgcjhvk");
+      System.out.println("affsgzdhtxfjgcjhvk");
+      System.out.println("affsgzdhtxfjgcjhvk");
+      System.out.println("affsgzdhtxfjgcjhvk");
+      System.out.println("affsgzdhtxfjgcjhvk");
    }
-
 }
